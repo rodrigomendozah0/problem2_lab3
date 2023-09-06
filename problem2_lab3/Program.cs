bc236b3146903cec039b69e446cc0f2163fa5377
Rodrigo <Rodrigo@HP_RODRIGO>
2023-09-05 23:17:47 -0500

    Agregar archivos de proyecto.

---
+++ problem2_lab3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem2_lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dias, categoria, desayuno;
+            float pago, pago_total;
+            Console.WriteLine("Cantidad de dias de estancia en el hotel: ");
+            dias = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la categoria del hotel (3/5) estrellas: ");
+            categoria = int.Parse(Console.ReadLine());
+            pago = 0f;
+
+            switch (categoria)
+            {
+                case 3:
+                    if (dias >= 1 && dias <= 5)
+                    {
+                        pago = 100f;
+                    }
+                    else if (dias >= 6 && dias <= 10)
+                    {
+                        pago = 85f;
+                    }
+                    else
+                    {
+                        pago = 70f;
+                    }
+                    break;
+                case 5:
+                    if (dias >= 1 && dias <= 5)
+                    {
+                        pago = 300f;
+                    }
+                    else if (dias >= 6 && dias <= 10)
+                    {
+                        pago = 270f;
+                    }
+                    else
+                    {
+                        pago = 250f;
+                    }
+                    break;
+            }
+            desayuno = dias * 7;
+            pago_total = (pago * dias) + desayuno;
+            Console.WriteLine("El pago total será: S/" + pago_total.ToString("0.00"));
+            Console.ReadKey();
+        }
+    }
+}
